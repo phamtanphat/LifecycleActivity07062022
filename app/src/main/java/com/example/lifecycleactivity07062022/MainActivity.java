2fb2009372ba69a2a4e85ed61d88fe074418ebd4
Phat <phatdroid94@gmail.com>
2022-08-02 20:59:42 +0700

    update code

--- app/src/main/java/com/example/lifecycleactivity07062022/MainActivity.java
+++ app/src/main/java/com/example/lifecycleactivity07062022/MainActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNavigateMain2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNavigateMain2 = findViewById(R.id.button_navigate_main2);
+
+        btnNavigateMain2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         Log.d("BBB","MainActivity: onCreate");
     }
 
